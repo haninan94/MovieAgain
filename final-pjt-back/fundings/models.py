8af85e9073d5f4a62090daf8e0cb5d4db89d58f3
yooooonzzzzzang <cherishvalue@naver.com>
2022-11-17 16:40:27 +0900

    [BE] feat: funding 모델 작성

--- final-pjt-back/fundings/models.py
+++ final-pjt-back/fundings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Funding(models.Model):
+    # 펀딩작성유저
+    user = models.ForeignKey(settings.AUTH_USER_MODE, on_delete=models.CASCADE)
+    goal_moeny = models.IntegerField()
+    minimum_money = models.IntegerField()
+    poster_path = models.CharField(max_length=220, null=True)
+    expired_date = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
+    movie_title = models.CharField(max_length=100)
+    content = models.TextField()
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODE, on_delete=models.CASCADE)
+    funding = models.ForeignKey(Funding, on_delete=models.CASCADE)
+    content = models.TextField()
+    star_rate = models.FloatField()
+
+class Backers(models.Model):
+    funding = models.ForeignKey(Funding, on_delete=models.CASCADE)    
+    user = models.ForeignKey(settings.AUTH_USER_MODE, on_delete=models.CASCADE)
+    donation = models.IntegerField(default=0)
