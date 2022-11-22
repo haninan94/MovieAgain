e369dbf6e7d051485fb2df7bdb7c0ea112d8e378
haninan94 <yuyu1172@gmail.com>
2022-11-22 09:45:39 +0900

    [BE] fix: Funding 댓글 전체 출력하는 버그 수정

--- final-pjt-back/fundings/views.py
+++ final-pjt-back/fundings/views.py
@@ -84,6 +84,7 @@ def comment_list(request):
 
 
 @api_view(['GET', 'DELETE', 'PUT'])
+@permission_classes([IsAuthenticated])
 def comment_detail(request, comment_pk):
     # comment = Comment.objects.get(pk=comment_pk)
     comment = get_object_or_404(Comment, pk=comment_pk)
