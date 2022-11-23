a474c8f43566a2536e0c48889330f6341972022f
한인환 <yuyu1172@gmail.com>
2022-11-23 21:05:56 +0900

    [BE] git: git pull

--- final-pjt-front/src/views/FundingDetailView.vue
+++ final-pjt-front/src/views/FundingDetailView.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="fundingdetail">
-    <div class="container">
+  <div id="funding-detail">
+    <div class="nes-container is-rounded is-dark">
       <div>
-        <img :src="funding?.poster_path" alt="" />
+        <img class="funding-img" :src="funding?.poster_path" alt="" />
       </div>
       <div>
         <div>
@@ -12,21 +12,38 @@
         </div>
         <div>
           <template>
-            <progress
+            <!-- <progress
               progress
               class="nes-progress is-primary"
               :value="funding.now_money"
               :max="funding.goal_money"
-            ></progress>
+            ></progress> -->
+            <div>
+              <b-progress
+                :value="(funding.now_money / funding.goal_money) * 100"
+                variant="info"
+                striped
+                :animated="animate"
+                class="mt-2"
+              ></b-progress>
+            </div>
           </template>
           <h1>{{ funding.now_money }}원 / {{ funding.goal_money }}원</h1>
-          <h1>{{ funding.now_money / funding.goal_money * 100 }}% 달성!!</h1>
-          {{ remainDate }}일 남았다!!
+          <h1>
+            {{ Math.ceil((funding.now_money / funding.goal_money) * 100) }}%
+            달성!!
+          </h1>
+          <h3>D-{{ remainDate }}일</h3>
+          <FundingDonateForm
+            :fundingMinimumMoney="this.funding.minimum_money"
+            :fundingId="funding.id"
+          />
         </div>
       </div>
     </div>
-    <router-link :to="{ name: 'FundingView' }">뒤로가기</router-link>
-    <FundingDonateForm/>
+    <router-link :to="{ name: 'FundingView' }">
+      <button class="nes-btn is-error">뒤로 가기</button>
+    </router-link>
     <FundingCommentForm :fundingId="this.funding.id" ref="FundingCommentForm" />
   </div>
   <!-- <template v-if="check">
@@ -36,14 +53,12 @@
           <progress class="nes-progress" :value="fundingRate" max="1"></progress>
         </template> -->
 </template>
-
 <script>
+import axios from "axios";
 import FundingCommentForm from "@/components/FundingCommentForm";
-import FundingDonateForm from '@/components/FundingDonateForm'
-import dayjs from 'dayjs'
-
+import FundingDonateForm from "@/components/FundingDonateForm";
+import dayjs from "dayjs";
 const API_URL = "http://127.0.0.1:8000";
-
 export default {
   name: "FundingDetailView",
   components: {
@@ -53,7 +68,8 @@ export default {
   data() {
     return {
       fundingMoney: 0,
-      remainDate: '',
+      remainDate: "",
+      animate: true,
     };
   },
   computed: {
@@ -70,14 +86,17 @@ export default {
       axios({
         method: "get",
         url: `${API_URL}/api/v2/fundings/${this.$route.params.id}`,
-      })
-        .then((res) => {
-          const expiredDate = res.data.expired_date.split("-").map(str => Number(str))
-          const todayDate = dayjs().format("YYYY-MM-DD").split("-").map(str => Number(str))
-          const remainDate = (new Date(expiredDate) - new Date(todayDate))
-          this.remainDate = remainDate / 1000 / 60 / 60 / 24
-        })
-
+      }).then((res) => {
+        const expiredDate = res.data.expired_date
+          .split("-")
+          .map((str) => Number(str));
+        const todayDate = dayjs()
+          .format("YYYY-MM-DD")
+          .split("-")
+          .map((str) => Number(str));
+        const remainDate = new Date(expiredDate) - new Date(todayDate);
+        this.remainDate = remainDate / 1000 / 60 / 60 / 24;
+      });
     },
     // getFundingMoney(payload) {
     //   console.log("Yes")
@@ -86,7 +105,7 @@ export default {
   },
   created() {
     this.getFundingDetail();
-    this.getDday()
+    this.getDday();
   },
   updated() {
     this.$refs.FundingCommentForm.getFundingComments();
@@ -94,10 +113,17 @@ export default {
   },
 };
 </script>
-
 <style>
-.container {
+#funding-detail {
+  width: 100vw;
+  height: 300vh;
+}
+.nes-container {
   display: flex;
   justify-content: space-evenly;
+  align-items: center;
+}
+.funding-img {
+  width: 300px;
 }
 </style>
